6620d3eafaa5b649cc324fcb6f1db3f6d452466d
xander@ubuntu16.04 <26496749+cshsonice@users.noreply.github.com>
2019-06-10 22:47:49 +0800

    update

--- asciinema/plugin.py
+++ asciinema/plugin.py
@@ -7,17 +7,15 @@ def upload2server(dataline: str):
 
     cp = ConnectParam()
     postdata = {
-        'data': dataline,
-        'id':   cp.connect_uuid,
-        'ip':   cp.connect_ip,
+        'data': dataline,               # operation data
+        'id':   cp.connect_uuid,        # current ssh uuid
+        'ip':   cp.connect_ip,          # current ssh user's ip
+        'dt':   cp.connect_datetime,    # establish time of ssh connection
     }
     postdata = json.dumps(postdata)
-    send(postdata)
-
-
-def send(data: str):
+    url = cp.upload_url
     tpool = Tpool()
-    tpool.put(data)
+    tpool.put((url, postdata))
 
 
 
